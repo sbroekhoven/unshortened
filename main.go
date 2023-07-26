9a4b587690d52db06031e7ec247c63de829d7fdc
Sebastian Broekhoven <2205121+sbro101@users.noreply.github.com>
2023-07-26 21:51:20 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/sbroekhoven/goredirects"
+	"github.com/sbro101/goredirects"
 )
 
 type config struct {
